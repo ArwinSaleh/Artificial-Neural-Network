88d20b07c2c99ad913441d0400364fea404359fe
ArwinSaleh <arwinsaleh@hotmail.com>
2020-09-14 21:55:57 +0200

    commit

--- ANNHW1/HW1 - Stochastic Hopfield Network.py
+++ ANNHW1/HW1 - Stochastic Hopfield Network.py
@@ -1,8 +1,22 @@
 import numpy as np
+import random as rnd
 
 N = 200
 T = 2 * 10 ** 5
 beta = 2
+reps = 100
+
+
+def sigmoid(b):
+    z = 1 / (1 + np.exp(-2 * beta * b))
+    return z
+
+
+def random_number_dist(nr1, nr2, prob1, prob2):
+    numbers = [nr1, nr2]
+    distribution = [prob1, prob2]
+    rand_dist = rnd.choices(numbers, distribution)
+    return rand_dist[0]
 
 
 def generate_patterns(p):
@@ -26,12 +40,44 @@ def generate_weight(p, patterns):
     return w_matrix
 
 
+def main(p_matrix, w_matrix):
+    m_u = []  # Order parameter
+    for i in range(0, reps):
+
+        last_state = p_matrix[:, 1]
+        iterations = np.zeros(reps)
+
+        for j in range(0, T):
+            next_state = last_state
+
+            m = rnd.randint(0, N)  # Choose neuron randomly
+            b_m = np.dot(w_matrix[m, :], last_state)
+            p_b = sigmoid(b_m)
+
+            next_state[m] = random_number_dist(1, -1, p_b, 1 - p_b)
+            iterations[j] = (1 / N) * p_matrix[:, 1] * np.transpose(next_state)
+            last_state = next_state
+
+        m_u[i] = (1 / T) * sum(iterations)
+
+    m_u_avg = (1 / reps) * sum(m_u)
+
+    return m_u_avg
+
+
 def task1():
     p = 7
     p_matrix = generate_patterns(p)
-
     w_matrix = generate_weight(p, p_matrix)
-    print(w_matrix)
+    res = main(p_matrix, w_matrix)
+    print(res)
 
+
+def task2():
+    p = 45
+    p_matrix = generate_patterns(p)
+    w_matrix = generate_weight(p, p_matrix)
+    res = main(p_matrix, w_matrix)
 
 task1()
+# task2()
