f22a799e67dcccc00aa3a8687d6caad8961d7edd
Arwin Saleh <arwinsaleh@hotmail.com>
2020-09-26 21:44:12 +0200

    almost

--- ANNHW2/4dim_linear_separability.py
+++ ANNHW2/4dim_linear_separability.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import genfromtxt
 import pandas as pd
+import mpmath 
 
 x = genfromtxt('input_data_numeric.csv', delimiter=',')
 
@@ -21,6 +22,13 @@ t_D = [1, 1, -1, 1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1]
 t_E = [-1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1]
 t_F = [1, 1, 1, -1, 1, 1, 1, 1, -1, -1, -1, 1, 1, 1, -1, 1]
 
+def sgn(i):  # This method is used to define the signum function at signum(0).
+
+    if i == 0:
+        return 1
+    else:
+        return np.sign(i)
+
 
 def generate_weights():
     s = (16, 1)
@@ -30,38 +38,68 @@ def generate_weights():
 
     return weight_matrix
 
-
-def output_function(u, theta, weights_i, x_i_u):
-
-    sum = 0
-    for i in range(1, 4):
-        sum = sum + np.dot(weights_i[i-1], x_i_u[u, i])
-
-    return np.tanh(0.5 * (- theta + sum))
-
-
-def energy_function(theta, weights_i, x_i_u):
-    sum = 0
-    for i in range(0, len(index_u)):
-        sum = sum + (t_A[i] - output_function(i, theta, weights_i, x_i_u)) ** 2
-
-    return 0.5 * sum
-
-#def stochastic_gradient_descent():
+#def backpropagation():
 
 
 
 
-def main():
+def g_prime(b):
+    return (mpmath.sech(b))**2
+
+
+def output_u(b_u):
+
+    return np.tanh(b_u / 2)
+
+
+def b_u(theta, weights, x_u):
+    return (- theta + weights*x_u)
+
+
+
+def delta_w(n,output, target, b_u, x_u):
+    return (n * (target - output) * (1 - b_u**2) * x_u) / 2   # transpose
+
+def delta_theta(n, output, target, b_u):
+    return - n * (target - output) * (1 - b_u ** 2) / 2
+
+
+if __name__ == "__main__":
 
     weights = generate_weights()
-
-    n = 0.2    # Learning rate
+    n = 0.02    # Learning rate
     theta = np.random.uniform(-1, 1)    # Threshold
 
-    H = energy_function(theta, weights, x)
-    print(H)
+    t = t_A
+
+    iterations = 10**5
+    i = 0
+    output = np.zeros((16, 5))
+
+    
+    while i < iterations:
+
+        u = np.random.randint(0, 16)    # random component on index u
+        x_u = x[u, :]
+        b = b_u(theta, weights, x_u) 
+        target = t[u]
+        output = output_u(b)
+        
+        if (output[u] == t).all():
+            i = iterations
+            print("Linearly separable")
+
+        d_theta = delta_theta(n, output, target, b)
+        d_weights = delta_w(n, output, target, b, x_u)
+
+        theta = theta + d_theta
+        weights = weights + d_weights
+
+        i = i + 1
+        
+        if i == iterations:
+            print("Not linearly separable")
 
 
 
-main()
+
---
+++ ANNHW2/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+t = [-1, 1, -1, 1, -1, 1]
+
+u = np.random.randint(0, 6)
+
+print(t[u])
