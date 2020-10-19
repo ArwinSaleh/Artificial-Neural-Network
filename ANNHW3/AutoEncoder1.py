14f296a87488402c8ba2fe139164b769912fc43d
Arwin Saleh <arwinsaleh@hotmail.com>
2020-10-19 20:17:08 +0200

    Commit

--- ANNHW3/AutoEncoder1.py
+++ ANNHW3/AutoEncoder1.py
@@ -1,30 +1,18 @@
-from sklearn.utils import shuffle
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers, models, datasets
-from tensorflow.keras.datasets import mnist
-from tensorflow.python.eager.context import PhysicalDevice, device
-from tensorflow.python.keras import activations
-from tensorflow.python.keras.backend import relu, softmax
-from tensorflow.python.keras.engine import input_layer
-from tensorflow.python.keras.layers.core import Flatten
-from tensorflow.python.keras.layers.normalization import BatchNormalization
-from keras.optimizers import SGD
-from keras.utils.np_utils import to_categorical
-from sklearn.model_selection import KFold
-from matplotlib import pyplot as plt
-from numpy import mean, std
+import keras
+from keras import layers
+from keras.utils import to_categorical
+from keras.datasets import mnist
+from keras.callbacks import TensorBoard
 
-class Encoder:
+
+class AutoEncoder:
     def __init__(self):
         (self.train_X, self.train_y), (self.test_X, self.test_y) = mnist.load_data() 
-        self.image_input_layer = layers.InputLayer(input_shape=(28, 28, 1))
+        self.image_input_layer = keras.Input(shape=(784,))
         self.fully_connected_layer1 = layers.Dense(50, activation='relu')
         self.fully_connected_layer2 = layers.Dense(2, 'relu')   # Bottleneck layer
         self.fully_connected_layer3 = layers.Dense(784, activation='relu')
-        
-        
-    
+
     def load_mnist(self):
         training_shape = (self.train_X.shape[0], 28, 28, 1) # Single channel shape
         testing_shape = (self.test_X.shape[0], 28, 28, 1)
@@ -32,3 +20,20 @@ class Encoder:
         self.test_X = self.test_X.reshape(testing_shape)
         self.train_y = to_categorical(self.train_y)
         self.test_y = to_categorical(self.test_y)
+
+    def build_encoder(self):
+        self.fully_connected_layer1 = self.fully_connected_layer1(self.image_input_layer)
+        self.fully_connected_layer2 = self.fully_connected_layer2(self.fully_connected_layer1)
+        self.fully_connected_layer3 = self.fully_connected_layer3(self.fully_connected_layer2)
+
+        auto_encoder = keras.Model(self.image_input_layer, self.fully_connected_layer3)
+        auto_encoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+        auto_encoder.fit(self.train_X, self.train_X, epochs=800, batch_size=8192, shuffle=True, validation_data=(self.test_X, self.test_X), callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+
+def main():
+    encoder = AutoEncoder()
+    encoder.load_mnist()
+    encoder.build_encoder()
+
+        
